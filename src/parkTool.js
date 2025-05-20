3a61ac72d3ce38a2ffa7fcfd8a5f394a68040820
markfriesen1979 <markfriesen2014@gmail.com>
2025-05-20 03:04:11 -0700

    Add files via upload

---
+++ src/parkTool.js
@@ -0,0 +1,45 @@
+/* micropolisJS. Adapted by Graeme McCutcheon from Micropolis.
+ *
+ * This code is released under the GNU GPL v3, with some additional terms.
+ * Please see the files LICENSE and COPYING for details. Alternatively,
+ * consult http://micropolisjs.graememcc.co.uk/LICENSE and
+ * http://micropolisjs.graememcc.co.uk/COPYING
+ *
+ */
+
+import { BaseTool } from './baseTool';
+import { Random } from './random';
+import { ANIMBIT, BULLBIT, BURNBIT, CONDBIT } from "./tileFlags";
+import { TileUtils } from './tileUtils';
+import { DIRT, FOUNTAIN, WOODS2 } from "./tileValues";
+
+var makeTool = BaseTool.makeTool;
+var ParkTool = makeTool(function(map) {
+  this.init(10, map, true);
+});
+
+
+ParkTool.prototype.doTool = function(x, y, blockMaps) {
+  if (this._worldEffects.getTileValue(x, y) !== DIRT) {
+    this.result = this.TOOLRESULT_NEEDS_BULLDOZE;
+    return;
+  }
+
+  var value = Random.getRandom(4);
+  var tileFlags = BURNBIT | BULLBIT;
+  var tileValue;
+
+  if (value === 4) {
+    tileValue = FOUNTAIN;
+    tileFlags |= ANIMBIT;
+  } else {
+    tileValue = value + WOODS2;
+  }
+
+  this._worldEffects.setTile(x, y, tileValue, tileFlags);
+  this.addCost(10);
+  this.result = this.TOOLRESULT_OK;
+};
+
+
+export { ParkTool };
